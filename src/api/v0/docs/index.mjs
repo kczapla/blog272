f8898dc003130e50306f78c9cd1c5307f9e1133b
kczapla <k.czapla68@gmail.com>
2021-03-18 21:32:25 +0000

    Format fix

--- src/api/v0/docs/index.mjs
+++ src/api/v0/docs/index.mjs
@@ -2,7 +2,7 @@ import Router from "koa-router"
 import koa2SwaggerUi from "koa2-swagger-ui"
 import yaml from "js-yaml"
 import path from "path"
-import fs from 'fs'
+import fs from "fs"
 
 const { koaSwagger } = koa2SwaggerUi
 
@@ -12,9 +12,9 @@ export function makeDocsRouter() {
   router.get("/docs", async (ctx, next) => {
     const openapiDoc = path.join(path.dirname(""), "/docs/api/v0/openapi.yaml")
     try {
-        ctx.body = yaml.load(fs.readFileSync(openapiDoc.toString()))
+      ctx.body = yaml.load(fs.readFileSync(openapiDoc.toString()))
     } catch (e) {
-        console.log(e)
+      console.log(e)
     }
     await next()
   })
